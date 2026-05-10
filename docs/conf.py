095ebac402faa5fa2c15eb9e2fa03934007d6a0d
David Dimenna <davidd28@vt.edu>
2026-05-10 13:27:26 -0400

    Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@ author = 'David Dimenna'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser',]
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
@@ -24,7 +23,23 @@ master_doc = 'index'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+import os
+import sys
+# Add the project's src directory to sys.path so Sphinx can import montecarlo.
+sys.path.insert(0, os.path.abspath('../src'))
+extensions = [
+'sphinx.ext.autodoc',
+'sphinx.ext.autosummary',
+'myst_parser',
+]
+autosummary_generate = True
+autodoc_default_options = {
+'members': True,
+'imported-members': True,
+'undoc-members': True,
+'show-inheritance': True,
+}
+autodoc_mock_imports = ['numpy']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
